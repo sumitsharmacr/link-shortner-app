b0107888d1c05fbbb2e4e598e34e7d2e8a069459
Sudhir Taneja <xs2rishu@gmail.com>
2021-05-17 13:35:32 +0530

    added bitly api changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', shortenUrl: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,6 +15,27 @@ class App extends React.Component {
 
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.value);
+    let uri = encodeURI(this.state.value);
+    const ACCESS_TOKEN = 'df3da57af354180d30d6fecd5abab350bee48c52'
+    let url = `https://api-ssl.bitly.com/v4/shorten`;
+    let data = {
+      "group_guid": "Bl5h7aJzSZV",  
+      "domain": "bit.ly",  
+      "long_url": uri  
+    }
+    fetch(url,{
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((response) => response.json())
+    .then((data) =>{
+      alert("Shorted URL is: " +data.link);
+      this.setState({shortenUrl: data.link});
+    })
     event.preventDefault();
   }
 
@@ -22,7 +43,7 @@ class App extends React.Component {
     return (
       <div className="container flex h-screen justify-center items-center">
         <form onSubmit={this.handleSubmit}>
-          <label>
+          <label>git@github.com:sumitsharmacr/link-shortner-app.git
             Enter URL:
             <input type="text" value={this.state.value} onChange={this.handleChange} className="min-w-1/4 ml-2 mr-2 mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             placeholder="https://john@example.com"/>
@@ -30,7 +51,8 @@ class App extends React.Component {
           <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-wait" value="Submit" />
         </form>
         <div>
-          Output Copy Here: 
+          <a href={this.state.shortenUrl}>Output Copy Here: {this.state.shortenUrl}</a>
+          
         </div>
       </div>
     );
